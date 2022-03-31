30d8efdfa0ffc9ce1da93ea9a53a7b8b0a6533e0
Joshua Son <joshuason@Joshuas-MacBook-Pro.local>
2022-03-30 22:11:06 -0400

    added some basic HTML for QandA section

--- src/QandA/index.js
+++ src/QandA/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { StateContext, DispatchContext } from '../appState/index.js';
 
+import QAList from './QAList';
 export default function QandQ() {
-  return <div>QandA Section</div>;
+  const [, dispatch] = useContext(DispatchContext);
+  const [state] = useContext(StateContext);
+
+  useEffect(() => {
+    console.log(state.QA);
+  }, [state.QA]);
+
+  return (
+    <div>
+      <h1>Questions & Answers:</h1>
+      <input placeholder='Have a question? Search for answers...' />
+      {state.QA.main &&
+        state.QA.main.results
+          .slice(0, 4)
+          .map((q) => <QAList key={q.question_id} q={q} />)}
+    </div>
+  );
 }
-
-
 
 export const qAndAStateInit = (productId) => {
-  return [
-    ['main', '/qa/questions/', { product_id: productId }]
-  ]
-}
+  return [['main', '/qa/questions/', { product_id: productId, count: 10 }]];
+};
