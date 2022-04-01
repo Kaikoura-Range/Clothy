59a960e068f79285ac5a0164bc2e8ad1ed6ce792
Jonathan Ghee <jonathan.p.ghee@gmail.com>
2022-04-01 08:12:34 -0500

    saving work

--- src/RatingsReviews/index.js
+++ src/RatingsReviews/index.js
@@ -1,25 +1,66 @@
 
-import React, { useState } from 'react';
+import React from 'react';
 import moment from 'moment';
 
-function Review() {
+export default function RatingsReviews(props) {
   // if( state.dev.pref ) {
     // console.log('DEV- Review rendered')
   // }
+  const [fullSummary, setFullSummary] = React.useState(false);
 
+  function Recommended(props) {
+    if(props.input === 'true') {
+      return <div>Recommended!</div>
+    }
+  }
+  function SalesResponse(props) {
+    if(props.response) {
+      return <div>{props.response}</div>
+    }
+  }
+
+  function Images(props) {
+     return props.photos.map((photo) => {
+      return <img key={photo.id} src={photo.url} alt='' height="50" width="50"/>
+    });
+  }
+  function Star(props) {
+   return <div>star rating: {props.stars}</div>
+ }
+ function Helpfulness(props) {
+  return <div>Was this helpful?</div>
+}
+
+function SummaryBody(props) {
+  return (
+  <div>
+    <b>{props.summary}</b>
+   {fullSummary ? <div>{props.body}</div> : <div>{props.body.substring(0,250)}</div>}
+  </div>
+  )
+}
+
+    if(props.reviewData) {
     return (
         <div>
-            <div><b>Review Summary: 60 chars, bold</b></div>
-            <div>{moment().format("MMM Do YY")}</div>
-            <div>Reviewer name: if(verified purchaser)</div>
-            <div>if(Recommend this product)</div>
-            <div>if(Internal Sales response)</div>
-            <div>Review body. default 250 chars, link to "show more"</div>
-            <div>up to 5 images. default thumbnail, click to show full res</div>
-            <div>star rating</div>
-            <div>Was this helpful?</div>
+           {props.reviewData.results.map((review,id) => {
+            return (
+            <div key={id}>
+              <div>{moment(review.date).format("MMM Do YY")}</div>
+              <div>review name- {review.reviewer_name}</div>
+              <SummaryBody summary={review.summary} body={review.body} showFull={false}/>
+              <Recommended input={review.recommend.toString()}/>
+              <SalesResponse response={review.response}/>
+              <Images photos={review.photos}/>
+              <Star stars={review.rating}/>
+              <Helpfulness counter={review.helpfulness}/>
+            </div>
+            )
+          })}
+            
         </div>
     )
+        }
 };
 
 
@@ -29,5 +70,5 @@ export const reviewStateInit = (productId) => {
     ]
   }
 
-export default Review;
+
 
