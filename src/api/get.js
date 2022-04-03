94e7be2c70df5a98a7a59061147be5e5294a7ad7
Ian Bringe <imbringeHR@gmail.com>
2022-04-03 16:07:54 -0400

    Added a very basic localcache to reduce api calls from our client. Currently cleans up a saved value after two minutes but that value can be tweaked.

--- src/api/get.js
+++ src/api/get.js
@@ -1,48 +1,9 @@
 
 
-import axios from 'axios';
-import GIT_TOKEN from '../config/config.js'
-const CAMPUS_CODE = 'hr-rfe';
-const baseUrl = `https://app-hrsei-api.herokuapp.com/api/fec2/${CAMPUS_CODE}`;
+import request from './request';
 
 
-
-
-const headers = {
-  Authorization: GIT_TOKEN,
-};
-
-const buildGetOptions = (endpoint, params = {}) => {
-  return {
-    method: 'get',
-    params,
-    headers,
-    url: endpoint,
-    baseURL: baseUrl,
-  }
-};
-
-
-
-const runFetch = (options) => {
-  return axios(options)
-    .then((res) => {
-      return res.data
-    })
-    .catch(err => {
-      console.log('API GET fetch err: ', err)
-      console.log('input options', options)
-    })
-};
-
-
-
-
-
-
-const get = ((endpoint, params) => {
-  return runFetch(buildGetOptions(endpoint, params))
-});
+const get = request.get
 
 
 const getAllObj = (allGetOptions) => {
@@ -69,6 +30,7 @@ const getAllArray = (allGetOptions) => {
       console.log('input array ', allGetOptions)
     })
 }
+
 
 
 
@@ -101,10 +63,9 @@ const initProductDataFetch = (detailsGets, reviewsGets, QAGets, relatedGets ) =>
 }
 
 const getAllProductData = (productId) => {
+
   if (get.allProductDataEndpoints) {
     let newEndpoints = get.allProductDataEndpoints(productId)
-    console.log('\nallProductDataEndpoints')
-    console.log(newEndpoints)
     return getAll(newEndpoints)
   } else {
     return new Promise((rej, res) => {
