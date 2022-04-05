fe65ec3e391c25ae812601075b532ef8a8016454
Ian Bringe <imbringeHR@gmail.com>
2022-04-05 09:20:20 -0400

    Implemented the first iteration of user data persistance using localStorage. Currently saves only the product Id number as to keep stored data low and to not save sensitive info on the broswer.

--- src/RelatedProducts/methods.js
+++ src/RelatedProducts/methods.js
@@ -37,8 +37,8 @@ const getPhotoUrls = (styles) => {
 
 
 
-const getRelatedEndpoints = (relatedIds) => {
-  return relatedIds.reduce((memo, prodId) => {
+const getProductEndpoints = (productIds) => {
+  return productIds.reduce((memo, prodId) => {
     memo[prodId] = {
       product: [`/products/${prodId}`, {}],
       styles: [`/products/${prodId}/styles`, {}]
@@ -49,16 +49,16 @@ const getRelatedEndpoints = (relatedIds) => {
 
 
 
-export const initRelatedProducts = (relatedIds, currentProduct, setRelatedItemData) => {
-  relatedIds = relatedIds.filter((id, ind) =>  relatedIds.slice((ind + 1)).indexOf(id) === -1 && id !== currentProduct)
-  const endpoints = getRelatedEndpoints(relatedIds)
+export const initProductsFromIds = (productIds, currentProduct, setItemData) => {
+  productIds = productIds.filter((id, ind) =>  productIds.slice((ind + 1)).indexOf(id) === -1 && id !== currentProduct)
+  const endpoints = getProductEndpoints(productIds)
   api.get.all(endpoints)
     .then((getResult) => {
       const condensed = Object.values(getResult).map((values) => {
         var photos = getPhotoUrls(values.styles)
         return ({ ...values.product, photos })
       })
-      setRelatedItemData(condensed)
+      setItemData(condensed)
       })
 }
 
